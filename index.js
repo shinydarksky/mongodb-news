b6c07fdc949d2e7fd9f105339d2297916496c8c5
MK404xL <shinydark.sky@gmail.com>
2020-08-09 20:56:12 +0700

    Changer port

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = 3000 
 const pathData = 'mongodb://localhost:27017/mongodb-news'
 app.use(express.static('public'))
 app.set('views', './views')
 app.set('view engine', 'ejs')
-app.listen(port)
+app.listen(process.env.PORT ||port)
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 mongoose.connect(pathData, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
