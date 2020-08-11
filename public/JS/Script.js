c5c1697078b92e7aaf2cc2adb728a291d62fd116
MK404xL <shinydark.sky@gmail.com>
2020-08-12 00:36:54 +0700

    Container change

--- public/JS/Script.js
+++ public/JS/Script.js
@@ -5,4 +5,18 @@ $('docment').ready(()=>{
     $('.back').click(()=>{
         history.back()
     })
+
+
+    var bodyWidth = $('body').width()
+    
+    if(bodyWidth<400){
+        console.log(bodyWidth + '' + typeof bodyWidth)
+        $('.root').removeClass('container')
+        $('.root').addClass('container-fluid')
+    }
+    else{
+        console.log(bodyWidth + '' + typeof bodyWidth)
+        $('.root').addClass('container')
+        $('.root').removeClass('container-fluid')
+    }
 })          
